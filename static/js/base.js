7ae29dbce41b62fc24b7fd3b6b43451f25867f67
chiderlin <chiderlin36@gmail.com>
2021-06-20 12:07:32 +0800

    add products page & modify burger-overlay

---
+++ static/js/base.js
@@ -0,0 +1,14 @@
+let burger_overlay = document.querySelector(".burger-overlay");
+let body = document.querySelector("body");
+let openbtn = document.getElementById("openbtn");
+let closebtn = document.getElementById("closebtn");
+openbtn.addEventListener("click", ()=> {
+    // burger_overlay.style.transform = "translateX(100%)";
+    burger_overlay.style.height = "100%";
+});
+
+closebtn.addEventListener("click", ()=> {
+    // burger_overlay.style.transform = "translateX(200%)";
+    burger_overlay.style.height = "0%";
+});
+
