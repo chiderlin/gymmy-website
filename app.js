8832eaf9823cdb6a4807d0dd52d95d4f1481335b
chiderlin <chiderlin36@gmail.com>
2021-06-21 23:56:37 +0800

    change => node.js architecture

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+require('dotenv/config');
+
+
+app.set('views','./templates');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+
+
+app.get('/', (req, res) => {
+    return res.render('index');
+});
+
+app.get('/products', (req, res) => {
+    return res.render('products');
+});
+
+app.get('/booking', (req, res) => {
+    return res.render('booking');
+});
+
+app.get('/class/<id>', (req, res) => {
+    return res.render('class');
+});
+
+app.get('/member/<username>', (req, res) => {
+    return res.render('member');
+});
+
+app.get('/backstage', (req, res) => {
+    return res.render('backstage');
+});
+
+app.listen(3001);
