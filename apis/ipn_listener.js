d44c5dd5418c759a10d3c0a35d107963861dd01b
chiderlin <chiderlin36@gmail.com>
2021-07-03 14:47:24 +0800

    try ipn msg

--- apis/ipn_listener.js
+++ apis/ipn_listener.js
@@ -2,54 +2,82 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.post('/',(req,res)=>{
+router.post('/', async(req,res)=>{
     // 1. 200 response
     console.log('It works! 😀');
     res.status(200).send('OK');
     res.end();
 
-    // 2.
-    // const body = req.body || {};
-    // let postreq = 'cmd=_notify-validate';
-    // Object.keys(body).map((key)=>{
-    //     postreq = `${postreq}&${key}=${body[key]}`;
-    //     return key;
-    // })
+    // 2. post same ipn mag back
+    const body = req.body || {};
+    try {
+        const isValidated = await validate(body);
+        if(!isValidated){
+            console.error('Error validating IPN message.');
+            return
+        }
 
-    // const url = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr';
+        // IPN msg is validated
+        const transactionType = body.txn_type;
+        switch(transactionType) {
+            case 'subscr_payment':
+                const status = body.payment_status;
+                const amount = body.mc_gross;
+                console.log('subscr_payments',status)
+                break;
+            case 'subscr_signup':
+                const subscr_date = body.subscr_date
+                console.log(subscr_date);
+                break;
+            case 'subscr_cancel':
+                break;
+            case 'subscr_eot':
+                break;
+            case 'recurring_payment_suspended':
+                break;
+            case 'recurring_payment_suspended_due_to_max_failed_payment':
+                break;
+            default:
+                console.log('Unhandled transaction type: ', transactionType)
+        };
 
-    // axios({
-    //     method: "POST",
-    //     url:url,
-    //     headers: {
-    //         'Content-Length': postreq.length,
-    //     },
-    //     encoding: 'utf-8',
-    //     body: postreq
-    // }).then((result)=>{
-    //     console.log(result.status)
-    //     console.log(result);
-    //     if(result.substring(0,8) === 'VERIFIED') {
-            
-    //     }
-    // }).catch((err)=>{
-    //     console.log('err:',err);
-    // });
+    } catch(e) {
+        console.error(e);
+    };
+});
 
+function validate(body={}){
+    return new Promise((resolve, reject)=>{
+        let postreq = 'cmd=_notify-validate';
+        Object.keys(body).map((key)=>{
+            postreq = `${postreq}&${key}=${body[key]}`;
+            return key
+        })
+        const url = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr';
 
-
-})
+        axios({
+            method: "POST",
+            url:url,
+            headers: {
+                'Content-Length': postreq.length,
+            },
+            encoding: 'utf-8',
+            body: postreq
+        }).then((result)=>{
+            console.log(result.status)
+            console.log(result.data);
+            if(result.data.substring(0,8) === 'VERIFIED') {
+                resolve(true);
+            } else if (result.substring(0,7) === 'INVALID') {
+                reject(new Error('IPN Message is invalid.'));
+            } else {
+                reject(new Error('Unexpected response body.'));
+            }
+        }).catch((err)=>{
+            console.log('err:',err);
+            reject(new Error(err))
+        });
+    })
+}
 
 module.exports = router;
-function payByToken(post_data){
-    const url = 'https://sandbox.tappaysdk.com/tpc/payment/pay-by-token'
-    axios.post(url, post_data, {
-        headers:{
-            'Content-Type':'application/json',
-            'x-api-key': post_data.partner_key
-        },
-    }).then((result)=>{
-        console.log(result.data);
-        const data = result.data;
-    })
-};
