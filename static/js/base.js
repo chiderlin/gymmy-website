4e4bc822eefca7d79e8d670a844aac21a6cdab12
chiderlin <chiderlin36@gmail.com>
2021-07-24 14:46:51 +0800

    cancel news page

--- static/js/base.js
+++ static/js/base.js
@@ -3,6 +3,7 @@ const openbtn = document.getElementById('openbtn');
 const closebtn = document.getElementById('closebtn');
 const burger_menu = document.querySelectorAll('.menu-format');
 const big_menu = document.querySelectorAll('li');
+console.log(big_menu)
 let login_status = false;
 let login_user_info;
 init();
@@ -25,21 +26,21 @@ window.onscroll = ()=>{
 }
 
 // 登出按鈕
-big_menu[5].addEventListener('click', () => {
+big_menu[4].addEventListener('click', () => {
     logOut();
 });
-burger_menu[5].addEventListener('click', () => {
+burger_menu[4].addEventListener('click', () => {
     logOut();
 });
 
 // 會員中心按鈕
-big_menu[3].addEventListener('click', () => {
+big_menu[2].addEventListener('click', () => {
     if (login_status) {
         const username = login_user_info.data.name;
         window.location.href = `/member/${username}`;
     }
 });
-burger_menu[3].addEventListener('click', () => {
+burger_menu[2].addEventListener('click', () => {
     if (login_status) {
         const username = login_user_info.data.name;
         window.location.href = `/member/${username}`;
@@ -60,11 +61,11 @@ closebtn.addEventListener('click', () => {
 const overlay_login = document.querySelector('.overlay-login');
 const login_close_btn = document.getElementById('login-close-btn');
 // 登入/註冊按鈕
-big_menu[4].addEventListener('click', () => {
+big_menu[3].addEventListener('click', () => {
     overlay_login.style.display = 'block';
 });
 
-burger_menu[4].addEventListener('click', () => {
+burger_menu[3].addEventListener('click', () => {
     burger_overlay.style.height = '0%';
     overlay_login.style.display = 'block';
 });
@@ -214,48 +215,48 @@ function sendEmail(customer_data){
 // view
 function loginNavBar() {
     // big screen
-    big_menu[0].classList.remove('hide'); // 最新消息
-    big_menu[1].classList.remove('hide'); // 本月課程
-    big_menu[3].classList.remove('hide'); // 會員中心
-    big_menu[5].classList.remove('hide'); // 登出系統
-    big_menu[2].classList.add('hide'); // 會員方案
-    big_menu[4].classList.add('hide'); // 登入/註冊
+    // big_menu[0].classList.remove('hide'); // 最新消息
+    big_menu[0].classList.remove('hide'); // 本月課程
+    big_menu[2].classList.remove('hide'); // 會員中心
+    big_menu[4].classList.remove('hide'); // 登出系統
+    big_menu[1].classList.add('hide'); // 會員方案
+    big_menu[3].classList.add('hide'); // 登入/註冊
     // small screen
-    burger_menu[0].classList.remove('hide'); // 最新消息
-    burger_menu[1].classList.remove('hide'); // 本月課程
-    burger_menu[3].classList.remove('hide'); // 會員中心
-    burger_menu[5].classList.remove('hide'); // 登出系統
-    burger_menu[2].classList.add('hide'); // 會員方案
-    burger_menu[4].classList.add('hide'); // 登入/註冊
+    // burger_menu[0].classList.remove('hide'); // 最新消息
+    burger_menu[0].classList.remove('hide'); // 本月課程
+    burger_menu[2].classList.remove('hide'); // 會員中心
+    burger_menu[4].classList.remove('hide'); // 登出系統
+    burger_menu[1].classList.add('hide'); // 會員方案
+    burger_menu[3].classList.add('hide'); // 登入/註冊
 };
 
 function initRenderMenu(api_data) {
     if (api_data.data !== null) {
         // 登入狀態
         // big screen
-        big_menu[0].classList.remove('hide'); // 最新消息
-        big_menu[1].classList.remove('hide'); // 本月課程
-        big_menu[3].classList.remove('hide'); // 會員中心
-        big_menu[5].classList.remove('hide'); // 登出系統
+        // big_menu[0].classList.remove('hide'); // 最新消息
+        big_menu[0].classList.remove('hide'); // 本月課程
+        big_menu[2].classList.remove('hide'); // 會員中心
+        big_menu[4].classList.remove('hide'); // 登出系統
 
         // small screen
-        burger_menu[0].classList.remove('hide'); // 最新消息
-        burger_menu[1].classList.remove('hide'); // 本月課程
-        burger_menu[3].classList.remove('hide'); // 會員中心
-        burger_menu[5].classList.remove('hide'); // 登出系統
+        // burger_menu[0].classList.remove('hide'); // 最新消息
+        burger_menu[0].classList.remove('hide'); // 本月課程
+        burger_menu[2].classList.remove('hide'); // 會員中心
+        burger_menu[4].classList.remove('hide'); // 登出系統
     } else {
         // 未登入狀態
         // big screen
-        big_menu[0].classList.remove('hide'); // 最新消息
-        big_menu[1].classList.remove('hide'); // 本月課程
-        big_menu[2].classList.remove('hide'); // 會員方案
-        big_menu[4].classList.remove('hide'); // 登入/註冊
+        // big_menu[0].classList.remove('hide'); // 最新消息
+        big_menu[0].classList.remove('hide'); // 本月課程
+        big_menu[1].classList.remove('hide'); // 會員方案
+        big_menu[3].classList.remove('hide'); // 登入/註冊
 
         // small screen
-        burger_menu[0].classList.remove('hide'); // 最新消息
-        burger_menu[1].classList.remove('hide'); // 本月課程
-        burger_menu[2].classList.remove('hide'); // 會員方案
-        burger_menu[4].classList.remove('hide'); // 登入/註冊
+        // burger_menu[0].classList.remove('hide'); // 最新消息
+        burger_menu[0].classList.remove('hide'); // 本月課程
+        burger_menu[1].classList.remove('hide'); // 會員方案
+        burger_menu[3].classList.remove('hide'); // 登入/註冊
     }
 };
 
