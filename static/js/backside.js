f5e52f43c0afb45e1bce83032923ba768f4f89fe
chiderlin <chiderlin36@gmail.com>
2021-07-29 17:18:19 +0800

    fix backside cookie token

--- static/js/backside.js
+++ static/js/backside.js
@@ -6,7 +6,7 @@ let index = 1;
 let student_amount;
 let per_class_booking_student;
 let all_student_list;
-const token = document.cookie.split('=')[2];
+const token = document.cookie.split('=')[3];
 console.log(token)
 init();
 
